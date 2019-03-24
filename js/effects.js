73064fceeed48468eee5ce84ac26e0f41d484191
SubbuSwaroop <subbuswaroop3795@gmail.com>
2019-03-24 15:41:14 +0530

    Integrated love.html and funny.html with home page

--- js/effects.js
+++ js/effects.js
@@ -1,14 +1,19 @@
-$(document).ready(function(){
-	/*alert("hi");*/
-	$(".logo .logoimg").css({"width":"350px",
-							 "height":"100px"});
-	$(".love").css("opacity","1");
-	$(".friendship").css("opacity","1");
-	$(".funny").css("opacity","1");
-	$(".sad").css("opacity","1");
-	$(".cute").css("opacity","1");
-	$(".cool").css("opacity","1");
-	$(".motivational").css("opacity","1");
-	$(".sarcastic").css("opacity","1");
-	$(".religious").css("opacity","1");
+$(document).ready(function() {
+  /*alert("hi");*/
+  $(".logo .logoimg").css({ width: "350px", height: "100px" });
+  $(".love").css("opacity", "1");
+  $(".friendship").css("opacity", "1");
+  $(".funny").css("opacity", "1");
+  $(".sad").css("opacity", "1");
+  $(".cute").css("opacity", "1");
+  $(".cool").css("opacity", "1");
+  $(".motivational").css("opacity", "1");
+  $(".sarcastic").css("opacity", "1");
+  $(".religious").css("opacity", "1");
+  $(".box").click(function() {
+    alert(this.classList[1]);
+    $("#everything").hide();
+    let className = this.classList[1];
+    $("#contentContainer").load(className + ".html");
+  });
 });
